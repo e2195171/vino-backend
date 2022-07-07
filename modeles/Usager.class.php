9c111abf8887fdfec46bf2541c4a48b6373e857d
DmitriDV <ddudchenko@gmail.com>
2022-07-06 22:19:39 -0400

    Correction  d'usager.

--- modeles/Usager.class.php
+++ modeles/Usager.class.php
@@ -20,14 +20,28 @@ class Usager extends Modele {
 	public function getListeUser()
 	{
 		$rows = Array();
-        $requete ='SELECT * FROM vino__usager;'; 
-
+        $requete ='SELECT
+                    u.id as id_usager,
+                    u.nom as nom_usager,
+                    u.prenom,
+                    u.courriel,
+                    u.phone,
+                    u.adresse as adresse_usager,
+                    u.id_ville,
+                    u.mot_passe,
+                    u.image_url,
+                    v.nom
+                    FROM vino__usager u
+                    INNER JOIN vino__ville v ON u.id_ville = v.id
+                    '; 
+        
 		if(($res = $this->_db->query($requete)) ==	 true)
 		{
 			if($res->num_rows)
 			{
 				while($row = $res->fetch_assoc())
 				{
+					$row['nom'] = trim(utf8_encode($row['nom']));
 					$rows[] = $row;
 				}
 			}
@@ -37,7 +51,7 @@ class Usager extends Modele {
 			throw new Exception("Erreur de requête sur la base de donnée", 1);
 			//$this->_db->error;
 		}
-		return $rows;
+        return $rows;
 	}
 
     /**
